acc77e3bf2fe617b8b52d599ed44a6c05b490ff4
Methmina-1234 <s23010167@ousl.lk>
2025-07-01 14:23:18 +0530

    Initial Commit

--- app/src/main/java/com/s23010167/tailorease/TrousersDatabaseHelper.java
+++ app/src/main/java/com/s23010167/tailorease/TrousersDatabaseHelper.java
@@ -34,12 +34,12 @@ public class TrousersDatabaseHelper extends SQLiteOpenHelper {
                 COL_NAME + ", " + COL_DESCRIPTION + ", " + COL_IMAGE + ", " + COL_MEASUREMENTS + ") VALUES " +
 
                 "('Chino Pants', 'Versatile and casual with a modern fit. Great for office or daily wear.', 'pic_trouser1', 'Waist: 28-38 in | Length: 40 in')," +
-                "('Cargo Pants', 'Durable with multiple pockets. Ideal for outdoor or street-style looks.', 'pic_trouser1', 'Waist: 30-42 in | Adjustable cuffs')," +
-                "('Formal Trousers', 'Polished and tailored for professional settings and events.', 'pic_trouser1', 'Waist: 28-36 in | Length: 38-42 in')," +
-                "('Jogger Pants', 'Comfortable with an elastic cuff. Great for workouts and casual wear.', 'pic_trouser1', 'Waist: 28-40 in | Stretchable fabric')," +
-                "('Slim Fit Trousers', 'Snug and trendy fit. Suitable for modern formal or semi-formal looks.', 'pic_trouser1', 'Waist: 28-34 in | Slim leg cut')," +
-                "('Wide-Leg Trousers', 'Loose fit offering great comfort and movement. Stylish for relaxed outfits.', 'pic_trouser1', 'Waist: 30-44 in | Extra leg room')," +
-                "('Pleated Trousers', 'Classic design with front pleats. Often used in formal or vintage looks.', 'pic_trouser1', 'Waist: 28-40 in | Pleated front')");
+                "('Cargo Pants', 'Durable with multiple pockets. Ideal for outdoor or street-style looks.', 'pic_trouser2', 'Waist: 30-42 in | Adjustable cuffs')," +
+                "('Formal Trousers', 'Polished and tailored for professional settings and events.', 'pic_trouser3', 'Waist: 28-36 in | Length: 38-42 in')," +
+                "('Jogger Pants', 'Comfortable with an elastic cuff. Great for workouts and casual wear.', 'pic_trouser4', 'Waist: 28-40 in | Stretchable fabric')," +
+                "('Slim Fit Trousers', 'Snug and trendy fit. Suitable for modern formal or semi-formal looks.', 'pic_trouser5', 'Waist: 28-34 in | Slim leg cut')," +
+                "('Wide-Leg Trousers', 'Loose fit offering great comfort and movement. Stylish for relaxed outfits.', 'pic_trouser6', 'Waist: 30-44 in | Extra leg room')," +
+                "('Pleated Trousers', 'Classic design with front pleats. Often used in formal or vintage looks.', 'pic_trouser7', 'Waist: 28-40 in | Pleated front')");
     }
 
     @Override
